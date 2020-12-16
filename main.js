285d6e0b01ec0168d5642bad798d2ffd94dc5ccc
kevklash <kevkde@gmail.com>
2020-12-15 23:05:02 -0600

    Created menu and keyboard shortcuts. Created about window and calling it from the menu.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, globalShortcut } = require('electron')
 
 // Set environment
 process.env.NODE.ENV = 'development'
@@ -8,8 +8,11 @@ const isDev = process.env.NODE.ENV !== 'production' ? true : false
 // Console loggin the platform
 console.log(process.platform)
 const isWindows = process.platform == 'win32' ? true : false
+const isMac = process.platform == 'darwin' ? true : false
 
+// Application windows
 let mainWindow
+let aboutWindow
 
 function createMainWindow() {
     mainWindow = new BrowserWindow({
@@ -17,7 +20,8 @@ function createMainWindow() {
         width: 500,
         height: 600,
         icon: `${__dirname}/assets/icons/Icon_256x256.png`,
-        resizable: isDev ? true : false
+        resizable: isDev ? true : false,
+        // backgroundColor: 'white'
     })
 
     mainWindow.loadURL(`file://${__dirname}/app/index.html`)
@@ -27,6 +31,82 @@ function createMainWindow() {
     // mainWindow.loadFile('/app/index.html')
 }
 
-app.on('ready', createMainWindow)
+function createAboutWindow() {
+    aboutWindow = new BrowserWindow({
+        title: 'About ImageShrink',
+        width: 300,
+        height: 200,
+        resizable: false,
+        // frame: false // Remove control buttons(close, minimize, etc.)
+        // backgroundColor: 'white'
+    })
+
+    aboutWindow.loadFile(`${__dirname}/app/about.html`)
+    aboutWindow.removeMenu() // Remove menu
+}
+
+app.on('ready', () => {
+    createMainWindow()
+    const mainMenu = Menu.buildFromTemplate(menu)
+    Menu.setApplicationMenu(mainMenu)
+    // Global shortcut example(not required when using roles)
+    globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload() )
+    mainWindow.on('closed', () => mainWindow = null)
+})
+
+const menu = [
+    // Setting menu in case of using Mac using roles(submenu items can use roles too)
+    ...(isMac ? 
+        [{ 
+            label : app.name,
+            submenu: [
+                {
+                    label: 'About',
+                    click: createAboutWindow()
+                }
+            ] 
+        }] : [ 
+            
+         ]),
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Quit',
+                // Keyboard shortcuts
+                accelerator: 'CmdOrCtrl+W',
+                // Another way: isWindows ? 'Ctrl+W' : 'Command+W'
+                click: () => app.quit()
+            }
+        ]
+    },
+    // Creating a new menu and submenu item using roles
+    ...(isDev ? [
+            {
+                label: 'Developer',
+                submenu: [
+                    { role: 'reload' },
+                    { role: 'forcereload' },
+                    { type: 'separator' },
+                    { role: 'toggledevtools' }
+                ]
+            }
+        ] 
+        : 
+    []),
+    ...(!isMac ? [
+            {
+                label: 'About',
+                submenu: [
+                    {
+                        label: 'About ImageShrink',
+                        click: () => createAboutWindow()
+                    }
+                ]
+            }
+        ] 
+        : 
+    [])    
+]
 
 app.allowRendererProcessReuse = true
